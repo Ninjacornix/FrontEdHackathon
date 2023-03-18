6499ad06b038ca09044a6115de976aaee04adc95
Milan Vrankić <ninjacornix@endava-bootcamps-MacBook-Pro.local>
2023-03-18 12:32:16 +0100

    add landing page

---
+++ client/frontend/components/layout/landingPageNavbar.jsx
@@ -0,0 +1,12 @@
+export default function landingPageNavbar() {
+    // make navigation bar
+    return (
+        <nav className="bg-transparent border-dark-200 px-2 sm:px-4 py-2.5 rounded absolute w-screen">
+            <ul className="flex flex-row justify-between">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Login</a></li>
+            </ul>
+	    </nav>
+    )
+}
+
