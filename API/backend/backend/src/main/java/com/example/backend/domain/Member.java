bea4fd69d5e2eb03d589ed625b388c8be9ca9ffd
ianba <ian.balen6@gmail.com>
2023-03-18 12:40:02 +0100

    reinit user into member

--- API/backend/backend/src/main/java/com/example/backend/domain/Member.java
+++ API/backend/backend/src/main/java/com/example/backend/domain/Member.java
@@ -4,27 +4,25 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
-@Data
+@Entity
 @AllArgsConstructor
 @NoArgsConstructor
-@Entity
-public class User {
+@Data
+public class Member {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long Id;
     private String name;
     private String surname;
-    private String username;
-    private String password;
     private String email;
+    private String password;
+    private String role;
     private String phone;
-    private String Role;
+
 
 
 }
--- API/backend/backend/src/main/java/com/example/backend/repository/MemberRepository.java
+++ API/backend/backend/src/main/java/com/example/backend/repository/MemberRepository.java
@@ -1,9 +1,9 @@
 package com.example.backend.repository;
 
-import com.example.backend.domain.User;
+import com.example.backend.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface MemberRepository extends JpaRepository<Member, Long> {
 }
