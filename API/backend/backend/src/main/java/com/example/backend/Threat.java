1bf0fd2d1b475b1ec6af613b3fdd21d4851388a0
Maksim <madzarmaksim@gmail.com>
2023-03-18 12:13:46 +0100

    add threat

---
+++ API/backend/backend/src/main/java/com/example/backend/Threat.java
@@ -0,0 +1,17 @@
+package com.example.backend;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Threat {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String severity;
+    private String source;
+    private Float potentialImpact;
+}
